e73497e9ddf67f5da2a2750319c187f34c920249
gwiron <gw.iron@foxmail.com>
2016-03-30 14:16:33 +0800

    v1.0

---
+++ kdhd/static/mobile/js/module/init.js
@@ -0,0 +1,42 @@
+/*
+ *	本文件做一些全局的初始化‘
+ *	
+ * 	如头部菜单等
+ */
+
+
+;(function(){
+
+
+	//菜单初始化
+	var memu = {
+		onOff : false,
+		height : parseInt( $(".memu-pull-down li").css('height') ) * $(".memu-pull-down li").length,
+		move :	function (e){
+			if( this.onOff == false ){
+				$(".memu-pull-down").css({
+					'height' : this.height+'px',
+				});
+				this.onOff = true;
+			} else {
+				$(".memu-pull-down").css({
+					'height' : '0',
+				});
+				this.onOff = false;
+			}
+			
+		}
+	}
+	$(".main-memu").tap(function(e){
+		memu.move();
+		e.stopPropagation();
+	});
+
+	$(document).tap(function(){
+		if( memu.onOff == true ){
+			memu.move();
+		}
+	});
+
+
+}());
